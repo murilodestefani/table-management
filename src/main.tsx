547d85c61181d6e804a44c2593c677294645e22e
murilodestefani <murilo_destefani@hotmail.com>
2024-05-29 18:30:19 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "@styles/global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
-import { NextUIProvider } from "@nextui-org/react";
 import { AuthProvider } from "@context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
-    </NextUIProvider>
   </React.StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,6 +11,5 @@ export default {
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
