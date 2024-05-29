634243ae70b7ff43a30bc86db872c67841257cb1
murilodestefani <murilo_destefani@hotmail.com>
2024-05-28 22:08:21 -0400

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
+import { ArrowLeft } from "@phosphor-icons/react";
+
 export function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <section className="p-4">
+        <header className="flex items-center justify-between">
+          <p>Bem vindo 👋</p>
+          <button className="bg-white p-3 shadow-xl shadow-zinc-200">
+            <ArrowLeft weight="bold" className="text-xl text-[#666687]" />
+          </button>
+        </header>
+      </section>
     </main>
   );
 }
