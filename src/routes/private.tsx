cf7417fff60bcc78ba0b2a24cc4b3380f7201590
murilodestefani <murilo_destefani@hotmail.com>
2024-05-29 18:24:14 -0400

    update

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@context/AuthContext";
 
-export function Private({ children }) {
+interface PrivateProps {
+  children: ReactNode;
+}
+
+export function Private({ children }: PrivateProps) {
   const { isAuthenticated } = useAuth();
 
   return isAuthenticated ? children : <Navigate to="/login" />;
